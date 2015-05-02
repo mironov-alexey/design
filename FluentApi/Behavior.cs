6d54ea887aecd1121dcaeae34c9aab5a0c349682
Mironov Alexey <mironov.alexey.fiit.urfu@gmail.com>
2015-05-02 12:12:50 +0500

    Added PerfomanceMeasurer and forgotten Behavior

---
+++ FluentApi/Behavior.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FluentTask
+{
+    public class Behavior
+    {
+        private readonly List<Action> actions = new List<Action>();
+
+        public Behavior()
+        {
+        }
+        public Behavior(IEnumerable<Action> actions)
+        {
+            this.actions = actions.ToList();
+        }
+
+        public Behavior Say(string message)
+        {
+//            Console.WriteLine(message);
+            actions.Add(() => Console.WriteLine(message));
+            return this;
+        }
+
+        public Behavior UntilKeyPressed(Action<Behavior> inner)
+        {
+            actions.Add(() => { WaitForKeyPress(inner);});
+            return this;
+        }
+
+        private static void WaitForKeyPress(Action<Behavior> inner)
+        {
+            while (!Console.KeyAvailable)
+            {
+                var behaviour = new Behavior();
+                inner(behaviour);
+                behaviour.Execute();
+                Thread.Sleep(500);
+            }
+            Console.ReadKey();
+        }
+
+        public Behavior Jump(JumpHeight height)
+        {
+//            Console.WriteLine("Jump: " + height);
+            actions.Add(() => Console.WriteLine("Jump: " + height));
+            return this;
+        }
+
+        public Behavior Delay(TimeSpan timeSpan)
+        {
+//            Thread.Sleep(timeSpan);
+            actions.Add(() => Thread.Sleep(timeSpan));
+            return this;
+        }
+
+        public void Execute()
+        {
+            foreach (var action in actions)
+            {
+                action();
+            }
+        }
+    }
+}
--- PerfLogger/Program.cs
+++ PerfLogger/Program.cs
@@ -1,20 +1,42 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Linq;
 
 namespace PerfLogger
 {
+    public class PerfomanceMeasurer : IDisposable
+    {
+        public Stopwatch Watch;
+        private Action<long> printElapsedTime;
+        public PerfomanceMeasurer(Action<long> inner)
+        {
+            printElapsedTime = inner;
+            Watch = new Stopwatch();
+            Watch.Start();
+        }
+        public void Dispose()
+        {
+            Watch.Stop();
+            printElapsedTime(Watch.ElapsedMilliseconds);
+        }
+    }
+    public class PerfLogger
+    {
+        public static IDisposable Measure(Action<long> printElapsedTime)
+        {
+            return new PerfomanceMeasurer(printElapsedTime);
+        }
+    }
 	class Program
 	{
 		static void Main(string[] args)
 		{
-			/*
 			var sum = 0.0;
 			using (PerfLogger.Measure(t => Console.WriteLine("for: {0}", t)))
 				for (var i = 0; i < 100000000; i++) sum += i;
 			using (PerfLogger.Measure(t => Console.WriteLine("linq: {0}", t)))
 				sum -= Enumerable.Range(0, 100000000).Sum(i => (double)i);
 			Console.WriteLine(sum);
-			*/
 		}
 	}
 }
